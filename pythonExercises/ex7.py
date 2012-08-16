fff3124fce80f0937b1c54ef3adf94707ea7b669
deku <dekuben@gmail.com>
2012-08-16 17:39:27 +0100

    added ex7.py next version will not have comma at end of line 20

---
+++ pythonExercises/ex7.py
@@ -0,0 +1,17 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 #check what this does
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end5 = "s"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+
+#try removing comma at the end of next line for a laugh
+print end1 + end2 + end3 + end3 + end5 + end3,
+print end7 + end8 + end9 + end10 + end3 + end9
